a2860856791fe750e20f7c6805b2412e1eafee41
Sisor <smietzner@yahoo.de>
2020-07-23 21:20:08 +0200

    handwash done

--- src/App.svelte
+++ src/App.svelte
@@ -1,1 +1,14 @@
-<h1>Test</h1> 
+<script>
+    import HowTo from "./HowTo.svelte";
+    import Timer   from "./Timer.svelte";
+
+</script>
+
+<h1>Test</h1>
+<Timer/>
+<HowTo/>
+<style>
+    h1 {
+        text-align: center;
+    }
+</style>
