3f42182ce1ce832240e8153769e6e735246b4f30
Sisor <smietzner@yahoo.de>
2020-07-23 21:29:03 +0200

    ReadMe created

--- src/HowTo.svelte
+++ src/HowTo.svelte
@@ -1,8 +1,7 @@
 <script></script>
-<p>HowTo</p>
 <div bp="grid">
     <div  bp="offset-5@md 4@md 12@sm" >
-        <img src="how_to_handwash.gif" alt="how to handwash" />
+        <img src="how_to_handwash.gif" alt="how to handwash"/>
         <p>
             <a href="https://www.who.int/gpsc/clean_hands_protection/en/">picture source</a>
             <a href="https://www.freesound.org/people/metrostock99/sounds/345086/">sound source</a>
